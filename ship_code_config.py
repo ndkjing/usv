3f4eb754aeca5d4ed9d2f21b0f5e6ebcebcfa484
ndkjing <ndkjing1@gmail.com>
2021-12-20 16:10:56 +0800

    2021/12/20 整理文件

--- ship_code_config.py
+++ ship_code_config.py
@@ -3,7 +3,7 @@
 """
 import os
 # ship_code = 'XXLJC4LCGSCAHSD0DA000'
-ship_code = 'XXLJC4LCGSCSD1DA003'
+ship_code = 'XXLJC4LCGSCSD1DA002'
 root_path = os.path.dirname(os.path.abspath(__file__))
 # 保存视频播放地址
 save_token_path = os.path.join(root_path, 'statics', 'save_token.json')
